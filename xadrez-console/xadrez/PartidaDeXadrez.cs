9e6b56cd0d242cf012405b8006c17c91be48704f
geisondrake@gmail.com <geisondrake@gmail.com>
2021-04-02 16:13:35 -0300

    Jogada especial em promocao

--- xadrez-console/xadrez/PartidaDeXadrez.cs
+++ xadrez-console/xadrez/PartidaDeXadrez.cs
@@ -141,6 +141,21 @@ namespace xadrez
                 throw new TabuleiroException("Você não pode se colocar em xeque!");
             }
 
+            Peca p = tab.peca(destino);
+
+            // #Jogada especial promocao
+            if (p is Peao)
+            {
+                if ((p.cor == Cor.Branca && destino.linha ==0) || (p.cor == Cor.Preta) && destino.linha == 7) {
+                    p = tab.retirarPeca(destino);
+                    pecas.Remove(p);
+                    Peca dama = new Dama(tab, p.cor);
+                    tab.colocarPeca(dama, destino);
+                    pecas.Add(dama);
+                }
+            }
+
+
             if (estaEmXeque(adversaria(jogadorAtual)))
             {
                 xeque = true;
@@ -159,7 +174,6 @@ namespace xadrez
                 mudaJogador();
             }
 
-            Peca p = tab.peca(destino);
             
             // #Jogadaespecial em passant
             if (p is Peao && (destino.linha == origem.linha -2 || destino.linha == origem.linha + 2))
